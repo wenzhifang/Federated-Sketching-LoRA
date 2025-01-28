64a0249efa642211206f84457a8141a9c1f6dbd9
wenzhi fang <39112845+wenzhifang@users.noreply.github.com>
2025-01-28 02:20:02 -0500

    Update and rename run_CMU_glue.sh to run_main.sh

--- GLUE/run_main.sh
+++ GLUE/run_main.sh
@@ -3,5 +3,5 @@
 datasets=('sst2' 'mrpc' 'cola' 'qnli' 'mnli' 'qqp' 'rte')
 
 for dataset in "${datasets[@]}"; do
-    CUDA_VISIBLE_DEVICES=0 python main_cmu_het_lora_glue.py --lora_r 64 --dataset "$dataset"
+    CUDA_VISIBLE_DEVICES=0 python main_slora_glue_het.py --lora_r 64 --dataset "$dataset"
 done
