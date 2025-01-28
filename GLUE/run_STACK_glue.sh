749cfe0fdf5826a64b056095a1d8e6950f34b632
wenzhi fang <39112845+wenzhifang@users.noreply.github.com>
2025-01-28 02:17:50 -0500

    Update run_STACK_glue.sh

--- GLUE/run_STACK_glue.sh
+++ GLUE/run_STACK_glue.sh
@@ -3,5 +3,5 @@
 datasets=('sst2' 'mrpc' 'cola' 'qnli' 'mnli' 'qqp')
 
 for dataset in "${datasets[@]}"; do
-    CUDA_VISIBLE_DEVICES=0 python main_stack_het_lora_glue.py --lora_r 16 --dataset "$dataset"
+    CUDA_VISIBLE_DEVICES=0 python main_stack_het_lora_glue.py --lora_r 64 --dataset "$dataset"
 done
