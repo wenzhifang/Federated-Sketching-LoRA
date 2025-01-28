743682977f4cc750f95f020a8e688ecf240e54e4
wenzhi fang <39112845+wenzhifang@users.noreply.github.com>
2025-01-28 02:18:10 -0500

    Delete GLUE/run_STACK_glue.sh

--- GLUE/run_STACK_glue.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-datasets=('sst2' 'mrpc' 'cola' 'qnli' 'mnli' 'qqp')
-
-for dataset in "${datasets[@]}"; do
-    CUDA_VISIBLE_DEVICES=0 python main_stack_het_lora_glue.py --lora_r 64 --dataset "$dataset"
-done
