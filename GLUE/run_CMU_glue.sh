c968aca3b1ec770ca6096e751f7533d412940da3
wenzhi fang <39112845+wenzhifang@users.noreply.github.com>
2025-01-28 02:17:28 -0500

    Update run_CMU_glue.sh

--- GLUE/run_CMU_glue.sh
+++ GLUE/run_CMU_glue.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-datasets=('sst2' 'mrpc' 'cola' 'qnli' 'mnli' 'qqp')
+datasets=('sst2' 'mrpc' 'cola' 'qnli' 'mnli' 'qqp' 'rte')
 
 for dataset in "${datasets[@]}"; do
-    CUDA_VISIBLE_DEVICES=0 python main_cmu_het_lora_glue.py --lora_r 16 --dataset "$dataset"
+    CUDA_VISIBLE_DEVICES=0 python main_cmu_het_lora_glue.py --lora_r 64 --dataset "$dataset"
 done
